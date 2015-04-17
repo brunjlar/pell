f4bc662b17836704322a56212888a75a4d69919f
brunjlar <lbrunjes@yahoo.com>
2015-04-18 00:42:12 +0200

    first complete version

--- Math/NumberTheory/Moduli/SquareRoots.hs
+++ Math/NumberTheory/Moduli/SquareRoots.hs
@@ -2,28 +2,38 @@ module Math.NumberTheory.Moduli.SquareRoots (sqrts) where
 
 import Data.List (sort)
 import Math.NumberTheory.Primes.Factorisation (factorise)
-import Math.NumberTheory.Moduli (chineseRemainder)
+import Math.NumberTheory.Moduli (chineseRemainder, sqrtModPPList)
 
 chineseRemainders :: [([Integer], Integer)] -> [Integer]
 chineseRemainders = fst . go where
     go :: [([Integer], Integer)] -> ([Integer], Integer)
     go [] = ([0], 1)
-    go [(xs, m)] = (xs, m)
-    go ((xs, m) : xms) = (zs, lcm m m') where
-        (ys, m') = go xms
-        zs = do
-            x <- xs
-            y <- ys
-            case chineseRemainder [(x, m), (y, m')] of
-                Just z -> return z
-                Nothing -> []
+    go xsm = foldr1 f xsm where
+        f (xs, m) (ys, n) = (xys, lcm m n) where
+            xys = do
+                x <- xs
+                y <- ys
+                case chineseRemainder [(x, m), (y, n)] of
+                    Just z  -> return z
+                    Nothing -> []
 
 sqrtsPP :: Integer -> (Integer, Int) -> [Integer]
-sqrtsPP a (p, e) = [x | x <- [0 .. (m - 1)], (x * x - a) `mod` m == 0] where
-    m = p ^ e
+sqrtsPP 1 (2, 1)           = [1]
+sqrtsPP 0 (p, e)           = takeWhile (< p ^ e) $ map (* q) [0..] where
+                                q = p ^ f
+                                f = (if even e then e else succ e) `div` 2
+sqrtsPP a (p, e)
+    | a `mod`  p      /= 0 = sqrtModPPList a (p, e)
+    | a `mod` (p * p) /= 0 = []
+    | otherwise            = do
+                                x <- sqrtsPP (a `div` (p * p)) (p, e - 2)
+                                takeWhile (< m) [p * x + i * p ^ (e - 1) | i <- [0..]]
+--  | otherwise    = [x | x <- [0 .. (m - 1)], (x * x - a) `mod` m == 0] 
+    where
+        m = p ^ e
 
 sqrts :: Integer -> Integer -> [Integer]
 sqrts a m
     | a <  0       = error $ "a must not be negative, but a == " ++ show a ++ " < 0."
     | otherwise    = sort $ chineseRemainders $ map f $ factorise m where
-                        f (p, e) = (sqrtsPP a (p, e), p ^ e)
+                        f (p, e) = (sqrtsPP (a `mod` p ^ e) (p, e), p ^ e)
